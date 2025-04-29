092c2d726832deed3f8c36984ed4d3d552850e19
Alycia Halliday <hal22013@byui.edu>
2025-04-29 11:01:49 -0600

    assignment basics

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
-
+using System.Globalization;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+     {
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        while (guess != magicNumber)
+        //int count_number = int count_number + 1;
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Guess higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Guess lower");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it! Congrats!! it took ___ guesses!");
+            }
+
+        }                    
     }
 }
